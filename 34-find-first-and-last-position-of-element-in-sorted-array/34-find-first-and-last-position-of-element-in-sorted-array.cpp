1a370b06d9033ed6312477cc76551088af0ad84b
Harsh Agarwal <92772093+HKAgrwl@users.noreply.github.com>
2022-07-29 19:51:08 +0530

    Time: 10 ms (73.39%), Space: 13.7 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
+    
+    // for the first occurence we will check in <mid also if our nums[mid]==target, so the condition is hi=mid-1 in when our nums[mid]==target 
+    
+    // for the second occurence we will only check in right half of mid,which lo=mid+1 in nums[mid]==target
+    
+    int occurence(vector<int>&nums,int k,int target){
+        int res=-1;
+        int lo=0;
+        int hi=nums.size()-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            
+            if(nums[mid]==target) {
+                res=mid;
+                if(k==1){
+                hi=mid-1;}
+                else{
+                    lo=mid+1;
+                }
+            }
+            if(nums[mid]<target){
+                lo=mid+1;
+            }
+            else if(nums[mid]>target) /* else condition was unnecessarily decreasing hi to mid-1 in case of [3,3,3] target =3 so that is why i did else if ....*/
+                hi=mid-1;
+                
+            }
+        
+        return res;
+        
+    }
+    
+    
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
-        int start = 0;
-        int end = nums.size()-1;
-        if(nums.size()==0){
-            start=-1;
-            end=-1;
-            ans.push_back(start);
-            ans.push_back(end);
-            return ans;
-        }
-        while(nums[start] < target && start<nums.size()-1){
-            start++;
-        }
-        while(nums[end]>target && end>0){
-            end--;
-        }
-        if(nums[start]!=target || nums[end]!=target){
-            start=-1;
-            end=-1;
-        }
-        ans.push_back(start);
-        ans.push_back(end);
-        return ans;
+        vector<int>store;
+        // if(nums.size()==0) return {-1,-1};
+        // if(nums.size()==1 && target==nums[0]) return{0,0};
+        // if(nums.size()==1 && target!=nums[0]) return {-1,-1};
+        
+        int first=occurence(nums,1,target);
+        int last=occurence(nums,2,target);
+        store.push_back(first);
+        store.push_back(last);
+        return store;
+      
     }
 };
