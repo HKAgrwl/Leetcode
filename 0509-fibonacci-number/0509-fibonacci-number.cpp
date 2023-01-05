f74d757e999220092ad16285e9a13ca092b76d2d
Harsh Agarwal <92772093+HKAgrwl@users.noreply.github.com>
2023-01-05 15:51:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (54.74%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+       int fibo[n+1];
+       fibo[0]=0;
+       fibo[1]=1;
+        for(int i=2;i<=n;i++){
+            fibo[i] = fibo[i-1] + fibo[i-2];
+        }
+        return fibo[n];
+    }
+};
